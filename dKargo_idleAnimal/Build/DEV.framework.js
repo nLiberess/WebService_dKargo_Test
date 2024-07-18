a224290a57dec03fb9bd324ca6cc16dbb70e2a27
Liberess <145514586+nLiberess@users.noreply.github.com>
2024-07-18 15:03:14 +0900

    ㅇㅇ

--- dKargo_idleAnimal/Build/DEV.framework.js
+++ dKargo_idleAnimal/Build/DEV.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3675280: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3675341: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3675405: function() {return Module.webglContextAttributes.powerPreference;},  
- 3675463: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3675518: function($0) {performance.now = function() { return $0; };},  
- 3675566: function($0) {performance.now = function() { return $0; };},  
- 3675614: function() {performance.now = Module['emscripten_get_now_backup'];}
+  3675360: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3675421: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3675485: function() {return Module.webglContextAttributes.powerPreference;},  
+ 3675543: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3675598: function($0) {performance.now = function() { return $0; };},  
+ 3675646: function($0) {performance.now = function() { return $0; };},  
+ 3675694: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
