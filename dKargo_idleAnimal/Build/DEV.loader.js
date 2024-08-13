038cd6611b36a94e0a6b04e45edce7be4fb0202f
Liberess <145514586+nLiberess@users.noreply.github.com>
2024-08-13 19:38:09 +0900

    ㅇㅇ

--- dKargo_idleAnimal/Build/DEV.loader.js
+++ dKargo_idleAnimal/Build/DEV.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 61597917,
+    wasmFileSize: 61597945,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
